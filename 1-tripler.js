8179e35bc9bc8cb365ce6eab8c4699f08dc16aa6
Chamon562 <Chamon562@gmail.com>
2021-05-02 15:08:54 -0700

    Add for loop to loop through array and console.log to see the integers band called the tripler function, while using node to see the output.

--- 1-tripler.js
+++ 1-tripler.js
@@ -10,6 +10,13 @@ tripler([4, 1, 7]); // => [ 12, 3, 21 ]
 
 function tripler(array) {
     // loop through array
+    for(let i = 0; i < array.length; i++){
+        console.log(array[i])
+    }
     // find a way to multiply each integer in the array by 3
     // maybe can use forEach or map and multiply 3 to each integer in the array
+    
+
 }
+
+tripler([1,2,3])
