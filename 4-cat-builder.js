a20d879cc09acd112246b9404e05b374f517d686
Chamon562 <Chamon562@gmail.com>
2021-05-03 21:37:53 -0700

    [4-cat-builder.js] Add 2nd function for dog to return an object and put in my same 3 parameters but with different arguements.

--- 4-cat-builder.js
+++ 4-cat-builder.js
@@ -22,3 +22,15 @@ function catBuilder(name, color, toys) {
 
 const cat1 = catBuilder('Garfield', 'golden', ['scratching-post', 'yarn']);
 console.log(cat1)
+
+function dogBuilder(name, color, toy){
+    const dog = {
+        name: name,
+        color: color,
+        toy: toy,
+    }
+    return dog
+}
+
+const dog1 = dogBuilder("BobDog", "Cat Fish Grey", ["dog-bone", "digging-dirt"])
+console.log(dog1)
