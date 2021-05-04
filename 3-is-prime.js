a2e661e7c8a0ff5549a6371576ec6411887feafe
Chamon562 <Chamon562@gmail.com>
2021-05-03 19:52:11 -0700

    [3-is-prime.js] Add third primeIt function bwithout looking for reference and doing by memory.

--- 3-is-prime.js
+++ 3-is-prime.js
@@ -34,3 +34,14 @@ function primeIt(num){
 }
 
 console.log(primeIt(5))
+
+function primeTime(num){
+    for(let i = 2; i < num; i++){
+        if(num % i === 0){
+            return false
+        }
+    }
+    return num > 1
+}
+
+console.log(primeTime(9))
