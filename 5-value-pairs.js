b703b0c6b95fc81421f4becb88cb1ce101acd49b
Chamon562 <Chamon562@gmail.com>
2021-05-07 16:29:35 -0700

    [5-value-pairs.js] Add getCryptoValue function to retrieve crypto key value, create 2 new crypto objects with coin and current price and pass those into the getCryptoValue function along with the key in string form currentPrice and coin. Use for in loop and inside use if statement to specify if the key is equal to the string passed into the functin then return that value by going into the object and then push it into the new array.

--- 5-value-pairs.js
+++ 5-value-pairs.js
@@ -16,23 +16,23 @@ function valuePair(obj1, obj2, key) {
   let newArray = [];
   let newObject = [obj1, obj2]
   console.log(key)
-    for (key1 in newObject) {
-      let objLocations = newObject[key1].location
-      let objNames = newObject[key1].name
-      if(key === "name"){
-        console.log(objNames)
-        // console.log("line 24 inside if statement",key)
-        newArray.push(objNames)
-      } else if(key === "location")
+  for (key1 in newObject) {
+    let objLocations = newObject[key1].location
+    let objNames = newObject[key1].name
+    if (key === "name") {
+      console.log(objNames)
+      // console.log("line 24 inside if statement",key)
+      newArray.push(objNames)
+    } else if (key === "location")
       newArray.push(objLocations)
-      // console.log(objLocations)
-      // console.log(objNames)
-      // if(key1 === key){
-      //   newArray.push(objLocations)
-      // } else{
-      // }
-    }
-    return newArray
+    // console.log(objLocations)
+    // console.log(objNames)
+    // if(key1 === key){
+    //   newArray.push(objLocations)
+    // } else{
+    // }
+  }
+  return newArray
   // return newArray
 }
 const object1 = { name: 'One', location: 'Remote', age: 1 };
@@ -42,50 +42,50 @@ const object2 = { name: 'Two', location: 'San Francisco' };
 // console.log(valuePair(object1, object2, 'location'))
 console.log(valuePair(object1, object2, 'name'))
 
-const getValue = (person1, person2, key) =>{
+const getValue = (person1, person2, key) => {
   // first createa  new array to have new values pushed into to return
   let newArray = [];
   let allPersonObj = [person1, person2]
   console.log(allPersonObj)
   console.log(key)
-  for(key1 in allPersonObj){
+  for (key1 in allPersonObj) {
     let peopleNames = allPersonObj[key1].name
     let peopleLocations = allPersonObj[key1].location
     let peopleAge = allPersonObj[key1].age
     console.log(peopleNames)
-    if(key === "name"){
+    if (key === "name") {
       newArray.push(peopleNames)
-    } else if(key === "location"){
+    } else if (key === "location") {
       newArray.push(peopleLocations)
-    } else if ( key === "age"){
+    } else if (key === "age") {
       newArray.push(peopleAge)
     }
   }
   return newArray
 }
 
-const person1 = {name: "Jim", location: "Funky Town", age: 45}
-const person2 = {name: "Barny", location: "Spider Town", age: 7}
+const person1 = { name: "Jim", location: "Funky Town", age: 45 }
+const person2 = { name: "Barny", location: "Spider Town", age: 7 }
 
 // console.log(getValue(person1, person2, "name"))
 console.log(getValue(person1, person2, "location"))
 // console.log(getValue(person1, person2, "age"))
 
-function valueObject(obj1, obj2, key){
+function valueObject(obj1, obj2, key) {
   console.log(key)
   // make a newArray equal to empty to have the specified value pushed in when later returned
   let newArray = [];
   // make variable for combined objects to better letter used in for in loop and grabbing its value
   let bothObj = [obj1, obj2]
   // after having both obj use for in loop and use if statment for key to equal the third 
-  for (keyOne in bothObj){
+  for (keyOne in bothObj) {
     console.log(bothObj[keyOne].name)
-    if(key === "name"){
+    if (key === "name") {
       newArray.push(bothObj[keyOne].name)
-    } else if (key === "location"){
+    } else if (key === "location") {
       newArray.push(bothObj[keyOne].location)
     }
-    
+
   }
   return newArray
 }
@@ -93,34 +93,34 @@ function valueObject(obj1, obj2, key){
 console.log(valueObject(object1, object2, "name"))
 console.log(valueObject(object1, object2, "location"))
 
-const getTheValues = (car1, car2, key) =>{
+const getTheValues = (car1, car2, key) => {
   let newArray = []
   let bothObj = [car1, car2]
-  for(key1 in bothObj){
-    if(key === "make"){
+  for (key1 in bothObj) {
+    if (key === "make") {
       newArray.push(bothObj[key1].make)
-    } else if( key === "location"){
+    } else if (key === "location") {
       newArray.push(bothObj[key1].location)
 
-    } else if(key === "model"){
+    } else if (key === "model") {
       newArray.push(bothObj[key1].model)
     }
   }
   return newArray
 }
-const usCars={make: "Ford", model: "Explorer",location: "Texas"}
-const japanCars={make: "Honda", model: "Integra Type R", location: "Osaka"}
+const usCars = { make: "Ford", model: "Explorer", location: "Texas" }
+const japanCars = { make: "Honda", model: "Integra Type R", location: "Osaka" }
 console.log(getTheValues(usCars, japanCars, "make"))
 console.log(getTheValues(usCars, japanCars, "model"))
 console.log(getTheValues(usCars, japanCars, "location"))
 
-let getThemValues = (car1, car2, key) =>{
+let getThemValues = (car1, car2, key) => {
   let newArray = [];
   let mainObj = [car1, car2];
-  for(key1 in mainObj){
-    if(key === "make"){
+  for (key1 in mainObj) {
+    if (key === "make") {
       newArray.push(mainObj[key1].make);
-    } else if(key === "model"){
+    } else if (key === "model") {
       newArray.push(mainObj[key1].model)
     }
   }
@@ -129,3 +129,22 @@ let getThemValues = (car1, car2, key) =>{
 
 console.log(getThemValues(usCars, japanCars, "make"))
 console.log(getThemValues(usCars, japanCars, "model"))
+
+const crypto1 = { coin: "Bitcoin", currentPrice: "$" + 56000 }
+const crypto2 = { coin: "Doge", currentPrice: "$" + 0.7530 }
+
+const getCryptoValue = (crypt1, crypt2, key) => {
+  let newArray = [];
+  let mainObj = [crypt1, crypt2];
+  for(key1 in mainObj){
+    if(key === "coin"){
+      newArray.push(mainObj[key1].coin);
+    } else if(key === "currentPrice"){
+      newArray.push(mainObj[key1].currentPrice);
+    }
+  }
+  return newArray;
+}
+
+console.log(getCryptoValue(crypto1, crypto2, "currentPrice"))
+console.log(getCryptoValue(crypto1, crypto2, "coin"))
