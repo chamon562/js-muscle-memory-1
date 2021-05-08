a5972fa426d7ee34f9690255b7352221d156fad7
Chamon562 <Chamon562@gmail.com>
2021-05-07 21:36:21 -0700

    [7-adults.js] Add a variable name newArray to have the values of the ages over 18 pushed in. Add for loop to iterate through the array of objects. Then create variable for people[i] object. Add if statement to say if pplObj.age is greater than or equal to 18. Then push the pplObj.age data into the new array and then return the newArray to show an array of ages over 18 making them adults.

--- 7-adults.js
+++ 7-adults.js
@@ -16,5 +16,28 @@ adults(ppl); // => [ 'Khalid Robinson', 'Post Malone' ]
 ***********************************************************************/
 
 function adults(people) {
-  
+  const newArray = [];
+  for (let i = 0; i < people.length; i++) {
+    // console.log(ppl[i])
+    let pplObj = people[i]
+    // console.log(pplObj)
+    // for(peeps in pplObj){
+    //   console.log(pplObj)
+    // }
+    console.log(pplObj.age)
+    if (pplObj.age >= 18) {
+      newArray.push(pplObj.age)
+    }
+
+  }
+  return newArray
 }
+
+const ppl = [
+  { name: 'Khalid Robinson', age: 22 },
+  { name: 'Ariel Winter', age: 20 },
+  { name: 'Post Malone', age: 25 },
+  { name: 'Willow Smith', age: 17 }
+];
+
+console.log(adults(ppl))
