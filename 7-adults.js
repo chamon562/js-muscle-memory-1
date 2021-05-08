8ff1954c940a0cc7741862413a301250ab8ad887
Chamon562 <Chamon562@gmail.com>
2021-05-07 21:38:24 -0700

    [7-adults.js] Add more people inside the ppl array of bojects.

--- 7-adults.js
+++ 7-adults.js
@@ -37,7 +37,12 @@ const ppl = [
   { name: 'Khalid Robinson', age: 22 },
   { name: 'Ariel Winter', age: 20 },
   { name: 'Post Malone', age: 25 },
-  { name: 'Willow Smith', age: 17 }
+  { name: 'Willow Smith', age: 18 },
+  { name: 'Joe Jangle', age: 19 },
+  { name: 'Todd Harris', age: 17 },
+  { name: 'Jim Jimbo', age: 16 },
+  { name: 'Mutt Buncher', age: 12 },
+  { name: 'Glass Beak', age: 7 },
 ];
 
 console.log(adults(ppl))
