83779cca182a55ef1dba8708ee99b5cd690be5ec
Chamon562 <Chamon562@gmail.com>
2021-05-11 15:37:07 -0700

    [8-count-scores.js] Add solution by first making a new obj variable to be later populated. Loop through array of objects with forEach and made it the variable kv. If statement says that if the new object kv. name is true then the new object kv name should += the kv. score else make the new obj kv.name equal the score. Also add another function for practice by making a new array of objects with fruits.

--- 8-count-scores.js
+++ 8-count-scores.js
@@ -31,11 +31,11 @@ const peeps = [
 countScores(peeps); //=> { Pete: 4, Mike: 4, Dexter: 6 }
 ***********************************************************************/
 
-const ppl = [ 
-  {name: "Pete", score: 10},
-  {name: "Mike", score : 10},
-  {name: "Pete", score: -8},
-  {name: "Dexter", score: 12}
+const ppl = [
+  { name: "Pete", score: 10 },
+  { name: "Mike", score: 10 },
+  { name: "Pete", score: -8 },
+  { name: "Dexter", score: 12 }
 ];
 const peeps = [
   {name: "Pete", score: 2},
@@ -49,23 +49,51 @@ const peeps = [
 
 // create a a total of each person score  and then put that into an object?
 function countScores(people) {
-  const newObj = {}
-  for(let i = 0; i < people.length; i++){
-    console.log(people[i])
-    console.log(people[i].name)
-    console.log(people[i].score)
-    if(people[i].name === "Dexter"){
-      console.log(people[i].score)
+  let newObj = {};
+  people.forEach((kv)=>{
+    // console.log(kv.name)
+    // console.log(newObj[kv.score])
+    if(newObj[kv.name]){
+      newObj[kv.name] += kv.score
+      // console.log(newObj)
+    } else {
+      newObj[kv.name] = kv.score
     }
-  }
-  // people.forEach((ppl) =>{
-  // //  console.log(Object.values(ppl)) 
-  // // console.log(ppl.name)
-  // console.log(newObj[ppl.name] = ppl.score)
-  // // console.log(newObj[ppl.score] = ppl.name)
-  //   // console.log(newObj[ppl.name] = ppl.score)
-  // })
+  })
   return newObj
 }
 
 console.log(countScores(peeps))
+const countPpl = countScores(ppl)
+console.log(countPpl)
+
+const fruits = [
+  { name: "apples", amount: 4 },
+  { name: "pears", amount: 6 },
+  { name: "oranges", amount: 2 },
+  { name: "bananas", amount: 2 },
+  { name: "oranges", amount: 5, },
+  { name: "pears", amount: 8 },
+  { name: "apples", amount: 3 },
+  { name: "bananas", amount: 10 },
+  {},
+  { name: "pears", amount: 7 },
+  { name: "apples", amount: 5 },
+  { name: "mangos", amount: 1 },
+];
+
+const mergeFruits = (fruits)=>{
+  // create a new obj to be later populated with new score
+  let newBasket = {};
+  // use .forEach to loop through fruits
+  fruits.forEach((f) =>{
+    if(newBasket[f.name]){
+      newBasket[f.name] += f.amount
+    } else {
+      newBasket[f.name] = f.amount
+    }
+  })
+  return newBasket;
+}
+
+console.log(mergeFruits(fruits))
